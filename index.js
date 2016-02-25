2920e1d01d8140a5387f32334a7f157abe8de426
Davidson Fellipe <email@fellipe.com>
2016-02-25 04:29:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+#!/usr/bin/env node
 
+var Promise = require('bluebird');
+var xyz = Promise.promisifyAll(require('XYZ'));
+var reference = require('./reference');
+
+exports.getReference = function() {
+  return reference;
+};
+
+exports.output = function(url) {
+  return new Promise(
+    function (resolve, reject) {
+      resolve()
+    })
+    .then(function() {
+      return xyz(url)
+        .then(function (data) {
+          return {
+            'url': url,
+            'plugin': reference.plugin,
+            'sensors': data.ruleGroups.SPEED
+          };
+        }
+      );
+    });
+};
